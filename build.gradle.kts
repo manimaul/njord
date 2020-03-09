6486557ec659fb09085301189c8385064794f374
William Kamp <manimaul@gmail.com>
2020-03-08 20:26:58 -0700

    Project Init
    Initialize project with skeleton application.

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.3.61"
+    application
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    mainClassName = "io.madrona.njord.AppKt"
+}
---
+++ src/test/kotlin/io/madrona/njord/AppTest.kt
@@ -0,0 +1,11 @@
+package io.madrona.njord
+
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+class AppTest {
+    @Test fun testAppHasAGreeting() {
+        val classUnderTest = App()
+        assertNotNull(classUnderTest.greeting, "app should have a greeting")
+    }
+}

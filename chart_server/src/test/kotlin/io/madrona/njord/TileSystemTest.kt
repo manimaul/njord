5827ee2f5e80ef5e626f290044eeb2590a68bc1c
William Kamp <manimaul@gmail.com>
2020-06-07 17:30:34 -0700

    porting tilesystem from python WIP

---
+++ chart_server/src/test/kotlin/io/madrona/njord/TileSystemTest.kt
@@ -0,0 +1,18 @@
+package io.madrona.njord
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+
+internal class TileSystemTest {
+
+    @Test
+    fun testMapSize() {
+        val ts = TileSystem()
+        assertEquals(1, ts.mapSizeTiles(0))
+        assertEquals(2, ts.mapSizeTiles(1))
+        assertEquals(4, ts.mapSizeTiles(2))
+        assertEquals(8, ts.mapSizeTiles(3))
+        assertEquals(16, ts.mapSizeTiles(4))
+    }
+}

88d75cf979b60a83fd04bc3018bcb59509026bc7
William Kamp <manimaul@gmail.com>
2021-08-21 09:21:26 -0700

    tile json

--- chart_server/src/main/kotlin/io/madrona/njord/ChartServerApp.kt
+++ chart_server/src/main/kotlin/io/madrona/njord/ChartServerApp.kt
@@ -2,6 +2,7 @@ package io.madrona.njord
 
 import com.willkamp.vial.api.VialServer
 import io.madrona.njord.model.About
+import io.madrona.njord.model.tileJson
 import org.gdal.gdal.gdal
 
 
@@ -10,8 +11,9 @@ class ChartServerApp {
     fun serve() {
         gdal.AllRegister()
         gdal.SetConfigOption("OGR_S57_OPTIONS", "LNAM_REFS:ON,UPDATES:ON,SPLIT_MULTIPOINT:ON,PRESERVE_EMPTY_NUMBERS:ON,RETURN_LINKAGES:ON")
-        VialServer.create()
-                ///curl http://localhost:9000/v1/about | jq
+        val server = VialServer.create()
+        server
+                // curl -k https://localhost:9000/v1/about | jq
                 .httpGet("/v1/about") { request ->
                     request.respondWith {
                         it.setBodyJson(
@@ -21,7 +23,14 @@ class ChartServerApp {
                                 )
                         )
                     }
-                }.listenAndServeBlocking()
+                }
+                // curl -k https://localhost:9000/v1/tileJson | jq
+                .httpGet("/v1/tileJson") { request ->
+                    request.respondWith {
+                        it.setBodyJson(tileJson("${"127.0.0.1"}:${server.vialConfig.port}"))
+                    }
+                }
+                .listenAndServeBlocking()
     }
 }
 

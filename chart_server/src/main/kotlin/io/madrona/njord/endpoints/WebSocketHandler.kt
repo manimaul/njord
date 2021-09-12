4626df8a6593d8804e8522da4d25bf738f80c853
William Kamp <manimaul@gmail.com>
2021-09-12 09:44:00 -0700

    unzip charts

--- chart_server/src/main/kotlin/io/madrona/njord/endpoints/WebSocketHandler.kt
+++ chart_server/src/main/kotlin/io/madrona/njord/endpoints/WebSocketHandler.kt
@@ -6,8 +6,12 @@ import io.ktor.websocket.*
 import io.madrona.njord.ChartsConfig
 import io.madrona.njord.Singletons
 import io.madrona.njord.ext.KtorWebsocket
+import io.madrona.njord.ext.letTwo
 import io.madrona.njord.logger
+import io.madrona.njord.model.EncUpload
 import kotlinx.coroutines.*
+import java.io.File
+import java.util.zip.ZipFile
 
 class ChartWebSocketHandler(
     config: ChartsConfig = Singletons.config,
@@ -20,15 +24,17 @@ class ChartWebSocketHandler(
     override suspend fun handle(ws: DefaultWebSocketServerSession) {
         log.info("ws uri = ${ws.call.url()}")
         log.info("ws query keys = ${ws.call.request.queryParameters.names()}")
-        ws.send("1")
-        ws.send("2")
 
-        scope.launch {
-            delay(5000)
-            ws.close()
+        letTwo(
+            ws.call.request.queryParameters["uuid"],
+            ws.call.request.queryParameters.getAll("file")) { uuid, files ->
+            scope.launch {
+                ws.processFiles(EncUpload(files, uuid))
+            }
+        } ?: run {
+            ws.send("invalid query params")
+            ws.close(CloseReason(CloseReason.Codes.NORMAL, "uuid and or files not provided"))
         }
-
-        ws.send("3")
 
         for (frame in ws.incoming) {
             when (frame) {
@@ -37,6 +43,63 @@ class ChartWebSocketHandler(
                 }
                 else -> {}
             }
+        }
+    }
+
+    private suspend fun DefaultWebSocketServerSession.processFiles(encUpload: EncUpload) {
+        unzipFiles(encUpload)
+        // todo: process with gdal
+        close()
+    }
+
+    private suspend fun DefaultWebSocketServerSession.unzipFiles(encUpload: EncUpload) {
+        letTwo(encUpload.uuidDir(), encUpload.cacheFiles()) { dir, files ->
+            files.forEach { zipFile ->
+                ZipFile(zipFile).use { zip ->
+                    zip.entries().asSequence().forEach { entry ->
+                        zip.getInputStream(entry).use { input ->
+                            if (!entry.name.startsWith("__MACOSX")) {
+                                send("file: ${entry.name}")
+                                val outFile = File(dir, entry.name).apply {
+                                    log.info("file = $absolutePath")
+                                }
+                                outFile.parentFile?.let {
+                                    if (!it.exists()) {
+                                        it.mkdirs()
+                                    }
+                                }
+                                if (!entry.isDirectory) {
+                                    outFile.outputStream().use { output ->
+                                        input.copyTo(output)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun EncUpload.cacheFiles()  : List<File> {
+        return uuidDir()?.let { dir ->
+            files.mapNotNull { name ->
+                File(dir, name).takeIf {
+                    it.exists()
+                } ?: run {
+                    log.error("chart dir file does not exist $dir/$name")
+                    null
+                }
+            }
+        } ?: emptyList()
+    }
+
+    private fun EncUpload.uuidDir()  : File? {
+        return File(charDir, uuid).takeIf {
+            it.exists()
+        } ?: run {
+            log.error("chart dir does not exist $charDir/$uuid")
+            null
         }
     }
 }
--- chart_server/src/main/kotlin/io/madrona/njord/ext/EnumExt.kt
+++ chart_server/src/main/kotlin/io/madrona/njord/ext/EnumExt.kt
@@ -6,6 +6,22 @@ inline fun <reified T: Enum<T>> fromString(value: String?) : T? {
     }
 }
 
+inline fun <A, B, R> letTwo(v1: A?, v2: B?, block: (A, B) -> R) : R? {
+    return if (v1 != null && v2 != null) {
+        block(v1, v2)
+    } else {
+        null
+    }
+}
+
+inline fun <A, B, C, R> letThree(v1: A?, v2: B?, v3: C?, block: (A, B, C) -> R) : R? {
+    return if (v1 != null && v2 != null && v3 != null) {
+        block(v1, v2, v3)
+    } else {
+        null
+    }
+}
+
 inline fun <reified A: Enum<A>, reified B: Enum<B>, T> letFromStrings(v1: String?, v2: String?, block: ((A, B) -> T)) : T? {
     return fromString<A>(v1)?.let { v1t ->
         fromString<B>(v2)?.let { v2r ->

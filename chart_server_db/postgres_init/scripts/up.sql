14925eaaf3e0abf3c4b3f68c1335ec684b831b29
William Kamp <manimaul@gmail.com>
2021-09-30 06:17:31 -0700

    occlusion wip

--- chart_server_db/postgres_init/scripts/up.sql
+++ chart_server_db/postgres_init/scripts/up.sql
@@ -1,18 +1,19 @@
 CREATE TABLE charts
 (
-    id         BIGSERIAL PRIMARY KEY,
-    name       VARCHAR UNIQUE NOT NULL, -- DSID_DSNM
-    scale      INTEGER        NOT NULL, -- DSPM_CSCL
-    file_name  VARCHAR        NOT NULL, -- actual file name
-    updated    VARCHAR        NOT NULL, -- DSID_UADT
-    issued     VARCHAR        NOT NULL, -- DSID_ISDT
+    id         BIGSERIAL                PRIMARY KEY,
+    name       VARCHAR                  UNIQUE NOT NULL, -- DSID_DSNM
+    scale      INTEGER                  NOT NULL, -- DSPM_CSCL
+    file_name  VARCHAR                  NOT NULL, -- actual file name
+    updated    VARCHAR                  NOT NULL, -- DSID_UADT
+    issued     VARCHAR                  NOT NULL, -- DSID_ISDT
 
     -- Although these could be stored in th features table these we need some of this meta data in order to
     -- derive MINZ and MAXX when SCAMIN and SCAMAX are not defined. This allows us to NOT have to rely on insertion
     -- order.
-    zoom       INTEGER        NOT NULL, -- Best display zoom level derived from scale and center latitude
-    dsid_props JSONB          NOT NULL, -- DSID
-    chart_txt  JSONB          NOT NULL  -- Chart text file contents e.g. { "US5WA22A.TXT": "<file contents>" }
+    zoom       INTEGER                  NOT NULL, -- Best display zoom level derived from scale and center latitude
+    mcovr      GEOMETRY(GEOMETRY, 4326) NOT NULL,
+    dsid_props JSONB                    NOT NULL, -- DSID
+    chart_txt  JSONB                    NOT NULL  -- Chart text file contents e.g. { "US5WA22A.TXT": "<file contents>" }
 );
 
 -- indices
@@ -68,6 +69,51 @@ BEGIN
                        z_range
                 FROM features
                 WHERE layer = i AND geom && ST_Transform(ST_TileEnvelope(z, x, y), 4326) AND z <@ z_range
+            )
+            SELECT ST_AsMVT(mvtdata.*, i)
+            FROM mvtdata
+            INTO rec;
+            res := res || rec;
+        END LOOP;
+    RETURN res;
+END
+$BODY$;
+
+------------------------------------------------------------------
+-- https://postgis.net/docs/reference.html#Geometry_Processing
+-- https://postgis.net/docs/ST_Intersection.html
+-- https://postgis.net/docs/ST_Difference.html
+--
+-- 1 find the chart mcovr geometries within the tile envelope ordered by zoom
+-- 2
+--
+CREATE OR REPLACE FUNCTION public.concat_mvt_occluded(z INTEGER, x INTEGER, y INTEGER)
+    RETURNS BYTEA
+    LANGUAGE plpgsql
+AS
+$BODY$
+DECLARE
+    tileEnvelope  geometry;
+    g   geometry;
+    i   TEXT;
+    res BYTEA DEFAULT '';
+    rec BYTEA;
+BEGIN
+    tileEnvelope = ST_Transform(ST_TileEnvelope(z, x, y), 4326);
+    FOR g in SELECT mcovr FROM charts
+        LOOP
+
+        END LOOP;
+
+    FOR i IN SELECT DISTINCT layer from features
+        LOOP
+            WITH mvtdata AS (
+                SELECT ST_AsMvtGeom(geom, tileEnvelope) AS geom,
+                       layer                                                            AS name,
+                       props                                                            AS properties,
+                       z_range
+                FROM features
+                WHERE layer = i AND geom && tileEnvelope AND z <@ z_range
             )
             SELECT ST_AsMVT(mvtdata.*, i)
             FROM mvtdata

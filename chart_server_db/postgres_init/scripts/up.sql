2b48be71929d3f0d011aafff4fc88e132deed65f
William Kamp <manimaul@gmail.com>
2021-10-22 14:49:08 -0700

    comment unused sql view

--- chart_server_db/postgres_init/scripts/up.sql
+++ chart_server_db/postgres_init/scripts/up.sql
@@ -42,36 +42,36 @@ CREATE INDEX features_zoom_idx ON features USING GIST (z_range);
 
 ------------------------------------------------------------------
 
-CREATE OR REPLACE FUNCTION public.concat_mvt(z INTEGER, x INTEGER, y INTEGER)
-    RETURNS BYTEA
-    LANGUAGE plpgsql
-AS
-$BODY$
-DECLARE
-    i   TEXT;
-    res BYTEA DEFAULT '';
-    rec BYTEA;
-BEGIN
-    FOR i IN SELECT DISTINCT layer from features
-        LOOP
-            WITH mvtdata AS (
-                SELECT ST_AsMvtGeom(geom, ST_Transform(ST_TileEnvelope(z, x, y), 4326)) AS geom,
-                       layer                                                            AS name,
-                       props                                                            AS properties,
-                       z_range
-                FROM features
-                WHERE layer = i
-                  AND geom && ST_Transform(ST_TileEnvelope(z, x, y), 4326)
-                  AND z <@ z_range
-            )
-            SELECT ST_AsMVT(mvtdata.*, i)
-            FROM mvtdata
-            INTO rec;
-            res := res || rec;
-        END LOOP;
-    RETURN res;
-END
-$BODY$;
+-- CREATE OR REPLACE FUNCTION public.concat_mvt(z INTEGER, x INTEGER, y INTEGER)
+--     RETURNS BYTEA
+--     LANGUAGE plpgsql
+-- AS
+-- $BODY$
+-- DECLARE
+--     i   TEXT;
+--     res BYTEA DEFAULT '';
+--     rec BYTEA;
+-- BEGIN
+--     FOR i IN SELECT DISTINCT layer from features
+--         LOOP
+--             WITH mvtdata AS (
+--                 SELECT ST_AsMvtGeom(geom, ST_Transform(ST_TileEnvelope(z, x, y), 4326)) AS geom,
+--                        layer                                                            AS name,
+--                        props                                                            AS properties,
+--                        z_range
+--                 FROM features
+--                 WHERE layer = i
+--                   AND geom && ST_Transform(ST_TileEnvelope(z, x, y), 4326)
+--                   AND z <@ z_range
+--             )
+--             SELECT ST_AsMVT(mvtdata.*, i)
+--             FROM mvtdata
+--             INTO rec;
+--             res := res || rec;
+--         END LOOP;
+--     RETURN res;
+-- END
+-- $BODY$;
 
 ------------------------------------------------------------------
 -- https://postgis.net/docs/reference.html#Geometry_Processing
@@ -81,51 +81,51 @@ $BODY$;
 -- 1 find the chart covr geometries within the tile envelope ordered by scale
 -- 2
 --
-CREATE OR REPLACE FUNCTION public.concat_mvt_occluded(z INTEGER, x INTEGER, y INTEGER)
-    RETURNS BYTEA
-    LANGUAGE plpgsql
-AS
-$BODY$
-DECLARE
-    tileEnvelope geometry;
-    coverage     geometry;
-    chart        RECORD;
-    i            TEXT;
-    res          BYTEA DEFAULT '';
-    rec          BYTEA;
-BEGIN
-    tileEnvelope = ST_Transform(ST_TileEnvelope(z, x, y), 4326);
-    coverage = null;
-    FOR chart in SELECT id, covr FROM charts WHERE st_intersects(covr, tileEnvelope) ORDER BY scale
-        LOOP
-            coverage = CASE
-                           WHEN coverage IS NULL THEN st_intersection(chart.covr, tileEnvelope)
-                           ELSE st_union(chart.covr, coverage)
-                END;
-
-            FOR i IN SELECT DISTINCT layer from features WHERE chart_id = chart.id
-                LOOP
-                    WITH mvtdata AS (
-                        SELECT ST_AsMvtGeom(geom, tileEnvelope) AS geom,
-                               layer                            AS name,
-                               props                            AS properties,
-                               z_range
-                        FROM features
-                        WHERE layer = i
-                          AND chart_id = chart.id
-                          AND geom && tileEnvelope
-                          AND z <@ z_range
-                    )
-                    SELECT ST_AsMVT(mvtdata.*, i)
-                    FROM mvtdata
-                    INTO rec;
-                    res := res || rec;
-                END LOOP;
-
-            IF st_within(coverage, tileEnvelope) THEN EXIT;
-            END IF;
-
-        END LOOP;
-    RETURN res;
-END
-$BODY$;
+-- CREATE OR REPLACE FUNCTION public.concat_mvt_occluded(z INTEGER, x INTEGER, y INTEGER)
+--     RETURNS BYTEA
+--     LANGUAGE plpgsql
+-- AS
+-- $BODY$
+-- DECLARE
+--     tileEnvelope geometry;
+--     coverage     geometry;
+--     chart        RECORD;
+--     i            TEXT;
+--     res          BYTEA DEFAULT '';
+--     rec          BYTEA;
+-- BEGIN
+--     tileEnvelope = ST_Transform(ST_TileEnvelope(z, x, y), 4326);
+--     coverage = null;
+--     FOR chart in SELECT id, covr FROM charts WHERE st_intersects(covr, tileEnvelope) ORDER BY scale
+--         LOOP
+--             coverage = CASE
+--                            WHEN coverage IS NULL THEN st_intersection(chart.covr, tileEnvelope)
+--                            ELSE st_union(chart.covr, coverage)
+--                 END;
+--
+--             FOR i IN SELECT DISTINCT layer from features WHERE chart_id = chart.id
+--                 LOOP
+--                     WITH mvtdata AS (
+--                         SELECT ST_AsMvtGeom(geom, tileEnvelope) AS geom,
+--                                layer                            AS name,
+--                                props                            AS properties,
+--                                z_range
+--                         FROM features
+--                         WHERE layer = i
+--                           AND chart_id = chart.id
+--                           AND geom && tileEnvelope
+--                           AND z <@ z_range
+--                     )
+--                     SELECT ST_AsMVT(mvtdata.*, i)
+--                     FROM mvtdata
+--                     INTO rec;
+--                     res := res || rec;
+--                 END LOOP;
+--
+--             IF st_within(coverage, tileEnvelope) THEN EXIT;
+--             END IF;
+--
+--         END LOOP;
+--     RETURN res;
+-- END
+-- $BODY$;

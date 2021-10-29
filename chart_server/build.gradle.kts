1123022f7ac61b89a7495c95aafc2d78beaadc67
William Kamp <manimaul@gmail.com>
2021-10-29 14:38:34 -0700

    update gdal

--- chart_server/build.gradle.kts
+++ chart_server/build.gradle.kts
@@ -43,6 +43,6 @@ dependencies {
 
 tasks.named<JavaExec>("run") {
     if (Os.isFamily(Os.FAMILY_MAC)) {
-        jvmArgs = listOf("-Djava.library.path=/usr/local/Cellar/gdal/3.3.1_3/lib/")
+        jvmArgs = listOf("-Djava.library.path=/usr/local/Cellar/gdal/3.3.2_3/lib/")
     }
 }

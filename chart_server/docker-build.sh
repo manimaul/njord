f46a93f682c45c7f161726757dc555c286b24052
William Kamp <manimaul@gmail.com>
2021-11-06 14:05:16 -0700

    docker wip

---
+++ chart_server/docker-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -eux
+
+DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+pushd "$DIR/.."
+./gradlew :chart_server:installDist
+popd
+docker build -t "ghcr.io/manimaul/njord-chart-server:latest" -f "$DIR/Dockerfile" "$DIR/.."
+docker push "ghcr.io/manimaul/njord-chart-server:latest"

488728e73119da6c9a6d86b3fa11b7428bc2f981
William Kamp <manimaul@gmail.com>
2021-11-20 17:52:48 -0800

    gradle scripts

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,6 @@
-FROM gradle:7.2.0-jdk11 as builder
-
-WORKDIR /build
-COPY . .
-RUN gradle :chart_server:installDist
-
 FROM osgeo/gdal:ubuntu-full-3.3.3
 
-COPY --from=builder /build/chart_server/build/install /opt
+COPY chart_server/build/install /opt
 
 ENV JAVA_OPTS="-Dconfig.file=/opt/chart_server/application.conf -Dcharts.webStaticContent=/opt/chart_server/public"
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,30 @@ task("version") {
         println("group: ${project.group}")
     }
 }
+
+/**
+ * Convenience task for running the PostGIS database with docker-compose
+ * eg `./gradlew :runPostGis`
+ */
+task<Exec>("runPostgis") {
+    workingDir("chart_server_db")
+    commandLine("docker-compose", "up")
+}
+
+/**
+ * Build the container image. This much faster than a multi-stage docker build.
+ * eg `./gradlew :buildImage`
+ */
+task<Exec>("buildImage") {
+    dependsOn(":chart_server:installDist")
+    commandLine("docker", "build", "-t", "ghcr.io/manimaul/njord-chart-server:${project.version}", ".")
+}
+
+/**
+ * Build the container image. This much faster than a multi-stage docker build.
+ * eg `./gradlew :buildImage`
+ */
+task<Exec>("publishImage") {
+    dependsOn("buildImage")
+    commandLine("docker", "push", "ghcr.io/manimaul/njord-chart-server:${project.version}")
+}

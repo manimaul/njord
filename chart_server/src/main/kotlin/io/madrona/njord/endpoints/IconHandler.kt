04cef7297bedd6e1f5c047927c7d492e910d93d8
William Kamp <manimaul@gmail.com>
2022-06-17 15:14:01 -0700

    morfac

--- chart_server/src/main/kotlin/io/madrona/njord/endpoints/IconHandler.kt
+++ chart_server/src/main/kotlin/io/madrona/njord/endpoints/IconHandler.kt
@@ -16,9 +16,15 @@ class IconHandler(
 
     override suspend fun handleGet(call: ApplicationCall) {
         call.parameters["name"]?.let { name ->
-            spriteSheet.spriteImage(name)?.let { imageBytes ->
+            spriteSheet.spriteImage(name.stripExt())?.let { imageBytes ->
                 call.respondBytes(imageBytes, ContentType.Image.PNG)
             }
         } ?: call.respond(HttpStatusCode.NotFound)
     }
+}
+
+private fun String.stripExt() : String {
+    return lastIndexOf('.').takeIf { it > 0 }?.let {
+        substring(0, it)
+    } ?: this
 }
--- chart_server/src/main/kotlin/io/madrona/njord/layers/Morfac.kt
+++ chart_server/src/main/kotlin/io/madrona/njord/layers/Morfac.kt
@@ -6,18 +6,87 @@ class Morfac : SymbolLayerable() {
     override val key = "MORFAC"
 
     override fun layers(options: LayerableOptions) = sequenceOf(
-            Layer(
-                    id = "${key}_point",
-                    type = LayerType.SYMBOL,
-                    sourceLayer = key,
-                    filter = listOf(Filters.any, Filters.eqTypePoint),
-                    layout = Layout(
-                            symbolPlacement = Placement.POINT,
-                            iconImage = listOf("get", "SY"),
-                            iconAnchor = Anchor.BOTTOM,
-                            iconAllowOverlap = true,
-                            iconKeepUpright = true,
-                    )
+        Layer(
+            id = "${key}_area_fill",
+            type = LayerType.FILL,
+            sourceLayer = key,
+            filter = listOf(
+                Filters.all,
+                Filters.eqTypePolyGon
+            ),
+            paint = Paint(
+                fillColor = colorFrom("CHBRN")
             )
+        ),
+        Layer(
+            id = "${key}_area_line",
+            type = LayerType.LINE,
+            sourceLayer = key,
+            filter = listOf(
+                Filters.all,
+                Filters.eqTypePolyGon
+            ),
+            paint = Paint(
+                lineColor = colorFrom("CHBLK"),
+                lineWidth = 1f
+            )
+        ),
+        Layer(
+            id = "${key}_line_tie_up_wall",
+            type = LayerType.LINE,
+            sourceLayer = key,
+            filter = listOf(
+                Filters.all,
+                Filters.eqTypeLineString,
+                listOf(Filters.eq, "CATMOR", 4)
+            ),
+            paint = Paint(
+                lineColor = colorFrom("CSTLN"),
+                lineWidth = 2f
+            )
+        ),
+        Layer(
+            id = "${key}_line_chain_wire_cable",
+            type = LayerType.LINE,
+            sourceLayer = key,
+            filter = listOf(
+                Filters.all,
+                Filters.eqTypeLineString,
+                listOf(Filters.eq, "CATMOR", 6)
+            ),
+            paint = Paint(
+                lineColor = colorFrom("CHMGF"),
+                lineWidth = 1f,
+                lineDashArray = listOf(5f, 5f)
+            )
+        ),
+        Layer(
+            id = "${key}_line_default",
+            type = LayerType.LINE,
+            sourceLayer = key,
+            filter = listOf(
+                Filters.all,
+                Filters.eqTypeLineString,
+                listOf(Filters.notEq, "CATMOR", 6),
+                listOf(Filters.notEq, "CATMOR", 4),
+            ),
+            paint = Paint(
+                lineColor = colorFrom("CSTLN"),
+                lineWidth = 2f,
+            )
+        ),
+        Layer(
+            id = "${key}_point",
+            type = LayerType.SYMBOL,
+            sourceLayer = key,
+            filter = listOf(Filters.any, Filters.eqTypePoint),
+            layout = Layout(
+                symbolPlacement = Placement.POINT,
+                iconImage = listOf("get", "SY"),
+                iconAnchor = Anchor.BOTTOM,
+                iconAllowOverlap = true,
+                iconKeepUpright = true,
+            )
+        )
     )
 }

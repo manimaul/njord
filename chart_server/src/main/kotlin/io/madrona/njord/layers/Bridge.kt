de2fabce0563b05eb0d6db9ff3da8ded5eac6531
William Kamp <manimaul@gmail.com>
2022-06-17 22:24:29 -0700

    bridge

--- chart_server/src/main/kotlin/io/madrona/njord/layers/Bridge.kt
+++ chart_server/src/main/kotlin/io/madrona/njord/layers/Bridge.kt
@@ -6,44 +6,45 @@ import io.madrona.njord.model.*
 class Bridge : SymbolLayerable() {
 
     override fun layers(options: LayerableOptions) = sequenceOf(
-            Layer(
-                    id = "${key}_point",
-                    type = LayerType.SYMBOL,
-                    sourceLayer = key,
-                    filter = listOf(Filters.any, Filters.eqTypePoint),
-                    layout = Layout(
-                            symbolPlacement = Placement.POINT,
-                            iconImage = listOf("get", "SY"),
-                            iconAnchor = Anchor.BOTTOM,
-                            iconAllowOverlap = true,
-                            iconKeepUpright = true,
-                    )
+        Layer(
+            id = "${key}_line_bridge",
+            type = LayerType.LINE,
+            sourceLayer = key,
+            filter = listOf(
+                Filters.any,
+                Filters.eqTypeLineString,
+                Filters.eqTypePolyGon,
             ),
-            Layer(
-                    id = "${key}_line",
-                    type = LayerType.SYMBOL,
-                    sourceLayer = key,
-                    filter = listOf(Filters.any, Filters.eqTypePoint),
-                    layout = Layout(
-                            symbolPlacement = Placement.POINT,
-                            iconImage = listOf("get", "SY"),
-                            iconAnchor = Anchor.BOTTOM,
-                            iconAllowOverlap = true,
-                            iconKeepUpright = true,
-                    )
-            ),
-            Layer(
-                    id = "${key}_area",
-                    type = LayerType.SYMBOL,
-                    sourceLayer = key,
-                    filter = listOf(Filters.any, Filters.eqTypePoint),
-                    layout = Layout(
-                            symbolPlacement = Placement.POINT,
-                            iconImage = listOf("get", "SY"),
-                            iconAnchor = Anchor.BOTTOM,
-                            iconAllowOverlap = true,
-                            iconKeepUpright = true,
-                    )
+            paint = Paint(
+                lineColor = colorFrom("CHGRD"),
+                lineWidth = 4f,
             )
+        ),
+        Layer(
+            id = "${key}_symbol_opening_bridge",
+            type = LayerType.SYMBOL,
+            sourceLayer = key,
+            filter = listOf(
+                Filters.all,
+                Filters.eqTypeLineStringOrPolygon,
+            ),
+            layout = Layout(
+                symbolPlacement = Placement.LINE_CENTER,
+                iconImage = listOf("get", "SY"),
+                iconKeepUpright = true,
+            )
+        ),
+        Layer(
+            id = "${key}_point",
+            type = LayerType.SYMBOL,
+            sourceLayer = key,
+            filter = listOf(Filters.any, Filters.eqTypePoint),
+            layout = Layout(
+                symbolPlacement = Placement.POINT,
+                iconImage = listOf("get", "SY"),
+                iconAnchor = Anchor.BOTTOM,
+                iconKeepUpright = true,
+            )
+        ),
     )
 }

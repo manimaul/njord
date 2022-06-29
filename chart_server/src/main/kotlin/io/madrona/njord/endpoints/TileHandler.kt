96e12ffebb9447f4d81ec2b0845f9c2fa2fc7751
William Kamp <manimaul@gmail.com>
2022-06-29 08:15:48 -0700

    debug tile

--- chart_server/src/main/kotlin/io/madrona/njord/endpoints/TileHandler.kt
+++ chart_server/src/main/kotlin/io/madrona/njord/endpoints/TileHandler.kt
@@ -3,11 +3,15 @@ package io.madrona.njord.endpoints
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
+import io.madrona.njord.ChartsConfig
+import io.madrona.njord.Singletons
 import io.madrona.njord.ext.KtorHandler
 import io.madrona.njord.ext.letThree
 import io.madrona.njord.geo.TileEncoder
 
-class TileHandler : KtorHandler {
+class TileHandler(
+    private val chartsConfig: ChartsConfig = Singletons.config
+) : KtorHandler {
     override val route = "/v1/tile/{z}/{x}/{y}"
 
     override suspend fun handleGet(call: ApplicationCall) {
@@ -18,7 +22,11 @@ class TileHandler : KtorHandler {
         ) { x, y, z ->
             TileEncoder(x, y, z)
                 .addCharts()
-                //.addDebug()
+                .apply {
+                    if (chartsConfig.debugTile) {
+                        addDebug()
+                    }
+                }
                 .encode()
         }?.let {
             call.respondBytes(it, ContentType.Application.ProtoBuf)

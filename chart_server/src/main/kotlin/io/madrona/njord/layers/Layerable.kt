5138d0f3c18d903f131221a93ac29cffa204929a
William Kamp <manimaul@gmail.com>
2022-06-30 13:41:37 -0700

    fix DI ctor injection

--- chart_server/src/main/kotlin/io/madrona/njord/layers/Layerable.kt
+++ chart_server/src/main/kotlin/io/madrona/njord/layers/Layerable.kt
@@ -1,6 +1,7 @@
 package io.madrona.njord.layers
 
 import io.madrona.njord.Singletons
+import io.madrona.njord.geo.symbols.SymbolLayerLibrary
 import io.madrona.njord.model.ChartFeature
 import io.madrona.njord.util.logger
 import io.madrona.njord.model.Depth
@@ -8,6 +9,7 @@ import io.madrona.njord.model.Layer
 
 abstract class Layerable(
     private val autoSymbol: Boolean = false,
+    private val symbolLayerLibrary: SymbolLayerLibrary = Singletons.symbolLayerLibrary,
 ) {
     val log = logger()
     val key = javaClass.simpleName.uppercase()
@@ -17,7 +19,7 @@ abstract class Layerable(
 
     fun preTileEncode(feature: ChartFeature) {
         if (autoSymbol) {
-            val sy = Singletons.symbolLayerLibrary.symbol(key, feature.props)
+            val sy = symbolLayerLibrary.symbol(key, feature.props)
             feature.props["SY"] = sy
         }
         if (feature.layer == key) {

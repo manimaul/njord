d73aaad6e8b8d85972318b6ff3ebe5f1ffb35430
William Kamp <manimaul@gmail.com>
2023-01-06 10:09:09 -0800

    react router

--- chart_server_fe/src/App.js
+++ chart_server_fe/src/App.js
@@ -1,18 +1,28 @@
 import njord from './njord.png'
 import './App.css';
+import {Routes, Route, Outlet} from "react-router-dom";
+import React from "react";
+
+const Home = () => <header className="App-header">
+    <img src={njord} className="App-logo" alt="logo"/>
+    <p>
+        Njord ENC - Electronic Navigation Chart Server
+    </p>
+</header>;
+const About = () => <h1>About</h1>;
+const NoMatch = () => <h2>Nothing to see here!</h2>
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={njord} className="App-logo" alt="logo" />
-        <p>
-          Njord ENC - Electronic Navigation Chart Server
-        </p>
-
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Outlet />}>
+                    <Route index element={<Home />}/>
+                    <Route path="about" element={<About />}/>
+                    <Route path="*" element={<NoMatch/>}/>
+                </Route>
+            </Routes>
+        </div>
+    );
 }
-
 export default App;

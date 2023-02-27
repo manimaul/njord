933360b4247180de6d83ecd1ce247850fd0858c5
William Kamp <manimaul@gmail.com>
2023-02-27 09:20:04 -0800

    enc ts

--- chart_server_fe/src/components/Enc.tsx
+++ chart_server_fe/src/components/Enc.tsx
@@ -3,16 +3,21 @@ import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Accordion from 'react-bootstrap/Accordion';
 
-// eslint-disable-next-line import/no-webpack-loader-syntax
+//@ts-ignore
+//eslint-disable-next-line import/no-webpack-loader-syntax
 import maplibregl from '!maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import "./Enc.css"
-// eslint-disable-next-line import/no-webpack-loader-syntax
+//@ts-ignore
+//eslint-disable-next-line import/no-webpack-loader-syntax
 import MapLibreWorker from '!maplibre-gl/dist/maplibre-gl-csp-worker';
 import ChartQuery from './ChartQuery';
+// import {useParams} from "react-router-dom";
 
 maplibregl.workerClass = MapLibreWorker;
+
 export function Enc() {
+	// let {page} = useParams();
     const mapContainer = useRef(null);
     const map = useRef(null);
     const [lng] = useState(-122.44);
@@ -23,21 +28,22 @@ export function Enc() {
 
     useEffect(() => {
         if (map.current) return; //stops map from intializing more than once
-        map.current = new maplibregl.Map({
+        let newMap = new maplibregl.Map({
             container: mapContainer.current,
             style: '/v1/style/feet',
             center: [lng, lat],
             zoom: zoom
         });
-        map.current.addControl(new maplibregl.NavigationControl(), 'top-right');
+        newMap.addControl(new maplibregl.NavigationControl(), 'top-right');
         new maplibregl.Marker({color: "#FF0000"})
             .setLngLat([-122.4002, 47.27984])
-            .addTo(map.current);
-        
-        map.current.on('click', function(e) {
-			var features = map.current.queryRenderedFeatures(e.point);
+            .addTo(newMap);
+
+        newMap.on('click', function(e: any) {
+			let features = newMap.queryRenderedFeatures(e.point);
 			setShow(features);
 		});
+		map.current = newMap
 	});
 
 	function clipboard() {
@@ -61,7 +67,7 @@ export function Enc() {
 	);
 }
 
-function DisplayQuery(props) {
+function DisplayQuery(props: any) {
 	if (props.object) {
 		return(
 			<>

8bcaf003a090a7b56313ab56ffed4cde3424b9fe
William Kamp <manimaul@gmail.com>
2023-03-20 12:38:20 -0700

    delete filtered charts

--- chart_server_fe/src/components/ControlCharts.tsx
+++ chart_server_fe/src/components/ControlCharts.tsx
@@ -1,15 +1,14 @@
 import React, {useState} from "react";
 import {useRequest} from "../Effects";
 import {Table} from "react-bootstrap";
-import {Link, useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 import Button from "react-bootstrap/Button";
-import {setDestination, storeEncState} from "./Enc";
 import {useAdmin} from "./Admin";
+import Form from "react-bootstrap/Form";
 
 type ChartProps = {
     id: number;
     name: string;
-    bounds: Bounds;
 };
 
 export type Bounds = {
@@ -20,25 +19,62 @@ export type Bounds = {
 }
 
 export function ControlCharts() {
+    const [filter, setFilter] = useState("")
     const [charts, setCharts] = useState<Array<ChartProps>>([])
     const [reload] = useRequest("/v1/chart_catalog", setCharts)
     const [admin] = useAdmin()
 
-    async function deleleteChart(id: number) {
+    async function deleteChart(ids: number[]) {
         setCharts([])
-        let response = await fetch(
-            `/v1/chart?id=${id}&signature=${admin?.signatureEncoded}`,
-            {
-                method: "DELETE"
-            }
-        )
-        console.log(`delete chart ${id} = ${response.statusText}`)
+        await Promise.all(ids.map(id => {
+            return fetch(
+                `/v1/chart?id=${id}&signature=${admin?.signatureEncoded}`,
+                {
+                    method: "DELETE"
+                }
+            ).then(response => {
+                console.log(`delete chart ${id} = ${response.statusText}`)
+            })
+        }))
         reload()
+    }
+
+    function filteredCharts(): ChartProps[] {
+        return charts.filter(each => {
+            if (filter.length > 0) {
+                return each.name.includes(filter)
+            }
+            return true
+        })
     }
 
     return (
         <div>
             <h2>Installed S57 ENCs</h2>
+            {admin &&
+                <Form.Control
+                    autoFocus
+                    className="my-2"
+                    placeholder="Type to filter name..."
+                    onChange={(e) => setFilter(e.target.value)}
+                    value={filter}
+                />
+            }
+            {
+                admin && filter.length > 0 &&
+                <>
+                    <Button
+                        variant="outline-danger"
+                        size="sm" onClick={() => {
+                            let toDelete = filteredCharts().map(each => each.id)
+                            deleteChart(toDelete)
+                    }
+                    }>Delete All
+                    </Button>
+                    <br/>
+                    <br/>
+                </>
+            }
             <Table striped bordered hover variant="light">
                 <thead>
                 <tr>
@@ -49,7 +85,7 @@ export function ControlCharts() {
                 </tr>
                 </thead>
                 <tbody>
-                {charts.map((each, i) => {
+                {filteredCharts().map((each, i) => {
                     return (
                         <tr key={i}>
                             <td>{each.id}</td>
@@ -59,7 +95,7 @@ export function ControlCharts() {
                                     <Button
                                         variant="outline-danger"
                                         size="sm" onClick={() => {
-                                        deleleteChart(each.id)
+                                        deleteChart([each.id])
                                     }
                                     }>Delete
                                     </Button>

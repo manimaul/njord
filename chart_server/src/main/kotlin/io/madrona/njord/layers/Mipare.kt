c34f8712728b1fcb61a0d30ae70f331758f2f7e4
William Kamp <manimaul@gmail.com>
2023-06-12 21:43:11 -0700

    mipare layer

--- chart_server/src/main/kotlin/io/madrona/njord/layers/Mipare.kt
+++ chart_server/src/main/kotlin/io/madrona/njord/layers/Mipare.kt
@@ -2,10 +2,25 @@ package io.madrona.njord.layers
 
 import io.madrona.njord.model.ChartFeature
 
-class Mipare : LayerableTodo() {
-    //todo:
-    override fun preTileEncode(feature: ChartFeature) = super.preTileEncode(feature)
+/**
+ * Geometry Primitives: Point, Area
+ *
+ * Object: Military practice area
+ *
+ * Acronym: MIPARE
+ *
+ * Code: 83
+ */
+class Mipare : Layerable() {
+    private val lineColor = Color.CHMGD
+    override fun preTileEncode(feature: ChartFeature) {
+        feature.pointSymbol(Sprite.CHINFO06)
+        feature.lineColor(lineColor)
+    }
 
-    //todo:
-    override fun layers(options: LayerableOptions) = super.layers(options)
+    override fun layers(options: LayerableOptions) = sequenceOf(
+        pointLayerFromSymbol(),
+        areaLayerWithPointSymbol(),
+        lineLayerWithColor(color = lineColor, style = LineStyle.DashLine)
+    )
 }

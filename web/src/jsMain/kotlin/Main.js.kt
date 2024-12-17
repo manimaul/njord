ef5be1e46b66171894f3d8b2d5c2a022120e77f7
William Kamp <manimaul@gmail.com>
2024-12-17 15:53:22 -0800

    bootstrap

--- web/src/jsMain/kotlin/Main.js.kt
+++ web/src/jsMain/kotlin/Main.js.kt
@@ -5,5 +5,7 @@ external fun require(module: String): dynamic
 
 fun main() {
     require("maplibre-gl/dist/maplibre-gl.css")
+    require("bootstrap/dist/css/bootstrap.css")
+    require("bootstrap/dist/js/bootstrap.bundle.js")
     renderComposable(rootElementId = "root") { Router() }
 }

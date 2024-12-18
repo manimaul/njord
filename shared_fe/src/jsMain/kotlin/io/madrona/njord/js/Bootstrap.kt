927d88147f921e5a9dcfcd3b67af538421e9253d
William Kamp <manimaul@gmail.com>
2024-12-18 14:21:49 -0800

    modal fix

---
+++ shared_fe/src/jsMain/kotlin/io/madrona/njord/js/Bootstrap.kt
@@ -0,0 +1,17 @@
+package io.madrona.njord.js
+
+import org.w3c.dom.Element
+
+// https://getbootstrap.com/docs/5.3/components/modal/#passing-options
+@JsModule("bootstrap")
+@JsName("bootstrap")
+@JsNonModule
+external class Bootstrap {
+    class Modal(name: String) {
+        fun show()
+        fun hide()
+        companion object {
+            fun getOrCreateInstance(name: Element): Modal?
+        }
+    }
+}

9274878e98b05f3d11a1cb7f649be8e559dafdec
William Kamp <manimaul@gmail.com>
2024-12-23 07:17:30 -0800

    chart installer wip

--- shared_fe/src/jsMain/kotlin/io/madrona/njord/ui/ChartInstaller.js.kt
+++ shared_fe/src/jsMain/kotlin/io/madrona/njord/ui/ChartInstaller.js.kt
@@ -1,21 +1,121 @@
 package io.madrona.njord.ui
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.*
+import io.madrona.njord.model.ws.WsMsg
+import io.madrona.njord.viewmodel.ChartInstallState
 import io.madrona.njord.viewmodel.chartInstallViewModel
-import org.jetbrains.compose.web.dom.Div
-import org.jetbrains.compose.web.dom.Form
-import org.jetbrains.compose.web.dom.Label
+import io.madrona.njord.viewmodel.utils.Complete
+import io.madrona.njord.viewmodel.utils.Fail
+import io.madrona.njord.viewmodel.utils.Loading
+import io.madrona.njord.viewmodel.utils.Uninitialized
+import org.jetbrains.compose.web.attributes.*
+import org.jetbrains.compose.web.dom.*
+import org.w3c.dom.HTMLFormElement
+import org.w3c.xhr.FormData
 
 @Composable
 fun ChartInstaller() {
-    val state = chartInstallViewModel.flow.collectAsState()
-    Form {
-        Div(attrs = {classes("mb-3")}) {
-            Label(attrs = {
-            }) {
+    val state by chartInstallViewModel.flow.collectAsState()
+    when (val upload = state.encUpload) {
+        is Complete -> ChartInstallProgress(state)
+        is Fail -> ErrorDisplay(upload) { }
+        is Loading -> Progress("Uploading chart file", state.uploadProgress)
+        Uninitialized -> ChartInstallForm()
+    }
+}
 
+@Composable
+fun ChartInstallProgress(state: ChartInstallState) {
+    when (val wsMsg = state.info) {
+        is WsMsg.CompletionReport -> {
+            println("complete $wsMsg")
+            Text("$wsMsg")
+        }
+        is WsMsg.Error -> {
+            println("info $wsMsg")
+            Text("$wsMsg")
+        }
+        is WsMsg.Extracting -> {
+            val progress = (wsMsg.progress * 100.0).toInt()
+            Progress("Extracting chart file ${wsMsg.step} of ${wsMsg.steps}", progress)
+        }
+        is WsMsg.Info -> {
+            val progress = ((wsMsg.num.toDouble() / wsMsg.total.toDouble()) * 100.0).toInt()
+            Progress(wsMsg.message, progress)
+        }
+        null -> {
+            println("null wsMsg")
+            Text("null wsMsg")
+        }
+    }
+}
+
+@Composable
+fun Progress(
+    title: String,
+    progress: Int,
+) {
+    H6 { Text(title) }
+    Div(attrs = {
+        classes("progress")
+        attr("role", "progressbar")
+        attr("aria-label", title)
+        attr("aria-valuenow", "$progress")
+        attr("aria-valuemin", "0")
+        attr("aria-valuemax", "100")
+    }) {
+        Div(attrs = {
+            classes("progress-bar", "w-$progress")
+        })
+    }
+}
+
+@Composable
+fun ChartInstallForm() {
+    var url: String? by remember { mutableStateOf(null) }
+    var form: HTMLFormElement? by remember { mutableStateOf(null) }
+    Form(attrs = {
+        ref {
+            form = it
+            object : DisposableEffectResult {
+                override fun dispose() {
+                    form = null
+                }
             }
         }
+    }) {
+
+        Div(attrs = {
+            classes("mb-3")
+        }) {
+            Label(attrs = {
+                classes("form-label")
+                attr("for", "encZip")
+            }) {
+                Text("ENC Chart(s) Zip File")
+            }
+            Input(InputType.File, attrs = {
+                id("encZip")
+                accept("application/zip")
+                classes("form-control")
+                name("enczip")
+                onChange {
+                    println("onChange ${it.target.value}")
+                    url = it.target.value
+                }
+            })
+        }
+        Button(attrs = {
+            type(ButtonType.Submit)
+            url?.let {
+                classes("btn", "btn-primary")
+            } ?: classes("btn", "btn-primary", "disabled")
+            onClick {
+                it.preventDefault()
+                form?.let {
+                    chartInstallViewModel.upload(FormData(it))
+                }
+            }
+        }) { Text("Submit") }
     }
 }

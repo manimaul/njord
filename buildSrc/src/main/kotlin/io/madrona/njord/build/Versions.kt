b59daea550459b7c7162a9769bbb1c9878fb0c52
William Kamp <manimaul@gmail.com>
2025-01-07 16:35:37 -0800

    update ktor

--- buildSrc/src/main/kotlin/io/madrona/njord/build/Versions.kt
+++ buildSrc/src/main/kotlin/io/madrona/njord/build/Versions.kt
@@ -1,8 +1,8 @@
 const val kotlinVersion = "2.1.0"
-const val ktorVersion = "2.3.13"
+const val ktorVersion = "3.0.3"
 const val logbackVersion = "1.4.12"
 const val composeVersion = "1.6.11"
-const val coroutinesVersion = "1.8.0"
+const val coroutinesVersion = "1.9.0"
 
 // https://developer.android.com/build/releases/gradle-plugin
 const val agpVersion = "8.7.0"

526ee3ca24bdaf895a917ab9c7589869ebf8a451
William Kamp <manimaul@gmail.com>
2026-02-22 20:17:35 -0800

    lib zip

---
+++ libzip/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    kotlin("multiplatform")
+    id("maven-publish")
+}
+
+group = "io.madrona"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    val hostOs = System.getProperty("os.name")
+    val isArm64 = System.getProperty("os.arch") == "aarch64"
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
+        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
+        hostOs == "Linux" && isArm64 -> linuxArm64("native")
+        hostOs == "Linux" && !isArm64 -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    nativeTarget.apply {
+        compilations.getByName("main") {
+            cinterops {
+                val libzip by creating
+            }
+        }
+        binaries {
+            staticLib {
+                baseName = "zip"
+            }
+        }
+    }
+
+    sourceSets {
+        nativeMain.dependencies {
+            implementation(project(":shared"))
+        }
+        nativeTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+}
+
+// ./gradlew publishAllPublicationsToGitHubPackagesRepository
+// ./gradlew publishToMavenLocal
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/manimaul/ktzip")
+            credentials {
+                username = System.getenv("GH_USER")
+                password = System.getenv("GH_TOKEN")
+            }
+        }
+    }
+}

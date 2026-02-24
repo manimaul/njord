467687dd8ef73f16d03ccc8a32f26123839b95d0
William Kamp <manimaul@gmail.com>
2026-02-24 08:30:20 -0800

    SOUNDG property

--- libgdal/src/nativeMain/kotlin/Soundings.kt
+++ libgdal/src/nativeMain/kotlin/Soundings.kt
@@ -1,4 +1,8 @@
+import io.madrona.njord.geojson.FeatureBuilder
+import io.madrona.njord.geojson.Point
+import io.madrona.njord.geojson.Position
 import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
 
 data class DisplayDepths(
     val meters: Double,
@@ -30,22 +34,12 @@ fun displayDepths(meters: Double) : DisplayDepths {
     )
 }
 
-//fun MutableMap<String, JsonElement>.addSoundingConversions(meters: Double) {
-//    val depths = displayDepths(meters)
-//    this["METERS"] = meters.json // need to add to /control/symbols/OBSTRN/VALSOU
-//    this["METERS_W"] = depths.metersWhole.json
-//    this["METERS_T"] = depths.metersTenths.json
-//    this["FEET"] = depths.feet.json
-//    this["FATHOMS"] = depths.fathoms.json
-//    this["FATHOMS_FT"] = depths.fathomsFeet.json
-//}
-
-//fun FeatureBuilder.addSounding() {
-//    (geo as? Point)?.let {
-//        val meters = it.position.z ?: 0.0
-//        addProperty("METERS", meters)
-//        geo = Point(Position(it.position.longitude, it.position.latitude))
-//    } ?: run {
-//        println("unexpected geometry point ${geo?.type}")
-//    }
-//}
+fun MutableMap<String, JsonElement>.addSoundingConversions(meters: Double) {
+    val depths = displayDepths(meters)
+    this["METERS"] = meters.json // need to add to /control/symbols/OBSTRN/VALSOU
+    this["METERS_W"] = depths.metersWhole.json
+    this["METERS_T"] = depths.metersTenths.json
+    this["FEET"] = depths.feet.json
+    this["FATHOMS"] = depths.fathoms.json
+    this["FATHOMS_FT"] = depths.fathomsFeet.json
+}

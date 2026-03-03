b286dfec9bb1c5e671044f0543d1c7593727f30f
William Kamp <manimaul@gmail.com>
2026-03-02 19:41:30 -0800

    cleanup

--- chart_server_db/postgres_init/README.md
+++
@@ -1,5 +0,0 @@
-# Build & Deploy
-```shell
-docker build -t ghcr.io/manimaul/njord-init-postgis:latest .
-docker push ghcr.io/manimaul/njord-init-postgis:latest
-```
--- chart_server_db/postgres_init/scripts/down.sql
+++
@@ -1,9 +0,0 @@
-------------------------------------------------------------------
--- DROP TABLE IF EXISTS styles;
-------------------------------------------------------------------
-DROP TABLE IF EXISTS charts;
-------------------------------------------------------------------
-DROP TABLE IF EXISTS features;
-DROP INDEX IF EXISTS features_gist;
-DROP INDEX IF EXISTS features_idx;
-------------------------------------------------------------------

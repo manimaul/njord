2585ac5bd48102fdf3fe60d580a4333ce8d56ed3
William Kamp <manimaul@gmail.com>
2026-03-13 16:32:20 -0700

    url handler - stream

--- server/src/nativeMain/kotlin/io/madrona/njord/endpoints/EncUrlHandler.kt
+++ server/src/nativeMain/kotlin/io/madrona/njord/endpoints/EncUrlHandler.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalForeignApi::class)
+
 package io.madrona.njord.endpoints
 
 import File
@@ -9,16 +11,25 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
+import io.ktor.utils.io.readAvailable
 import io.madrona.njord.Singletons
 import io.madrona.njord.ext.KtorHandler
 import io.madrona.njord.model.EncUpload
 import io.madrona.njord.model.EncUrlRequest
 import io.madrona.njord.util.logger
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.usePinned
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
+import platform.posix.fclose
+import platform.posix.fopen
+import platform.posix.fwrite
+
+private const val CHUNK_SIZE = 8 * 1024 * 1024 // 8 MB
 
 class EncUrlHandler(
     val chartDir: File = Singletons.chartUploadDir,
@@ -35,16 +46,47 @@ class EncUrlHandler(
             ?: "chart_${Clock.System.now().toEpochMilliseconds()}.zip"
 
         launch {
+            val tmp = File(chartDir, "$fileName.tmp")
             runCatching {
                 log.info("downloading $url -> $fileName")
-                val tmp = File(chartDir, "$fileName.tmp")
                 val dest = File(chartDir, fileName)
-                val bytes = client.get(url).readRawBytes()
-                tmp.writeBytes(bytes)
+                val tmpPath = tmp.getAbsolutePath().toString()
+                val fp = fopen(tmpPath, "wb")
+                    ?: throw IllegalStateException("cannot open $tmpPath for writing")
+                var totalBytes = 0L
+                try {
+                    client.prepareGet(url).execute { response ->
+                        val contentLength = response.contentLength()
+                        val channel = response.bodyAsChannel()
+                        val buffer = ByteArray(CHUNK_SIZE)
+                        var lastLoggedPct = -1
+                        while (!channel.isClosedForRead) {
+                            val count = channel.readAvailable(buffer)
+                            if (count > 0) {
+                                buffer.usePinned { pinned ->
+                                    fwrite(pinned.addressOf(0), 1u, count.toULong(), fp)
+                                }
+                                totalBytes += count
+                                if (contentLength != null && contentLength > 0) {
+                                    val pct = (totalBytes * 100L / contentLength).toInt() / 10 * 10
+                                    if (pct > lastLoggedPct) {
+                                        log.info("downloading $fileName: $pct%")
+                                        lastLoggedPct = pct
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } finally {
+                    fclose(fp)
+                }
                 tmp.renameTo(dest.getAbsolutePath().toString())
-                log.info("download complete: $fileName (${bytes.size} bytes)")
+                log.info("download complete: $fileName ($totalBytes bytes)")
             }.onFailure {
                 log.error("failed to download $url: ${it.message}")
+                if (tmp.exists()) {
+                    tmp.deleteRecursively()
+                }
             }
         }
 
